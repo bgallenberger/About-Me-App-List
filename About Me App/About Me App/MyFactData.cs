eaf578d083b3be4c41c751a38b32a726e40485bc
Brian Gallenberger <BGallenberger@my.wctc.edu>
2020-11-23 19:55:33 -0600

    added images

--- About Me App/About Me App/MyFactData.cs
+++ About Me App/About Me App/MyFactData.cs
@@ -12,14 +12,15 @@ namespace About_Me_App
         public static IEnumerable<MyFactData> All { private set; get; }
         public string TheFact { get; set; }
         public string ShortFact { get; set; }
+        public string TheImage { get; set; }
         static MyFactData()
         {
             List<MyFactData> all = new List<MyFactData>();
-            all.Add(new MyFactData() { TheFact = "My name is Brian Gallenberger", ShortFact = "Name" });
-            all.Add(new MyFactData() { TheFact = "I went to Oconomowoc High School", ShortFact = "High School" });
-            all.Add(new MyFactData() { TheFact = "I am currently attending WCTC", ShortFact = "College" });
-            all.Add(new MyFactData() { TheFact = "I have 3 cats named Jack, Gavin, and Tessa", ShortFact = "Pets" });
-            all.Add(new MyFactData() { TheFact = "I like to play video games and watch movies and tv shows", ShortFact = "Hobbys" });
+            all.Add(new MyFactData() { TheFact = "My name is Brian Gallenberger", ShortFact = "Name", TheImage = "me.png" });
+            all.Add(new MyFactData() { TheFact = "I went to Oconomowoc High School", ShortFact = "High School", TheImage = "ohs.jpg" });
+            all.Add(new MyFactData() { TheFact = "I am currently attending WCTC", ShortFact = "College", TheImage = "wctc.png" });
+            all.Add(new MyFactData() { TheFact = "I have 3 cats named Jack, Gavin, and Tessa", ShortFact = "Pets", TheImage = "cats.jpg" });
+            all.Add(new MyFactData() { TheFact = "I like to play video games and watch movies and tv shows", ShortFact = "Hobbys", TheImage = "gamesAndMovies.jpg" });
             All = all;
 
         }
